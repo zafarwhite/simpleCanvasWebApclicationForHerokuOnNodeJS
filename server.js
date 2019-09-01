0c92e3c92e8ddb0a67de9d132bc45e0f254a07e4
Zafar <34389538+zafarwhite@users.noreply.github.com>
2019-09-01 09:06:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// const express = require('express');
-// const enableWs = require('express-ws');
-// let fs = require('fs');
+const express = require('express');
+const enableWs = require('express-ws');
+let fs = require('fs');
 
-// const app = express();
-// const expressWs = enableWs(app);
-// app.use(express.static('public'));
+const app = express();
+const expressWs = enableWs(app);
+app.use(express.static('public'));
 
 
-// app.listen(3000, function () {
-//     console.log('Server is working now, click http://127.0.0.1:3000');
-// });
+app.listen(3000, function () {
+    console.log('Server is working now, click http://127.0.0.1:3000');
+});
