744f4b17d10ab7dacc7666950053891aa6fc791f
Zafar Kudratov <zafarkudratov@gmail.com>
2019-09-02 16:54:18 +0300

    port

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const enableWs = require('express-ws');
 let fs = require('fs');
-
+// const URL = https://canvas2dgame.herokuapp.com;
 const app = express();
 const expressWs = enableWs(app);
 app.use(express.static('public'));
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
+
+app.listen(port, function () {
     console.log('Server is working now, click http://127.0.0.1:3000');
 });
